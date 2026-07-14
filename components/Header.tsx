3f49c04167964699f27dbdc71b9aaf69161ac0f7
NguyenThanhDuyOfficial <nguyenthanhduyofficial@gmail.com>
2026-07-14 21:45:46 +0700

    feat: update Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { MenuIcon } from "lucide-react"
 import Link from "next/link"
+import { Button } from "./ui/button"
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 z-50 h-16 w-full bg-[#f8f8f8] p-4 px-8 md:px-16 flex justify-between items-center">
@@ -8,7 +9,11 @@ export default function Header() {
         <Link href="/guides">Hướng dẫn</Link>
         <Link href="/blogs">Bài Blogs</Link>
       </div>
-      <MenuIcon />
+      <MenuIcon className="md:hidden" />
+      <div className="space-x-4">
+        <Link href="">Tham gia</Link>
+        <Button variant="ghost">Đăng nhập</Button>
+      </div>
     </header>
   )
 }
