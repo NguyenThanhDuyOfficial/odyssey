e9676ce75f5d7f982e2dc3471bfbff40fff44204
NguyenThanhDuyOfficial <nguyenthanhduyofficial@gmail.com>
2026-08-14 18:23:36 +0700

    refactor: add src dir

--- apps/web/.source/browser.ts
+++ apps/web/.source/browser.ts
@@ -7,6 +7,6 @@ const create = browser<typeof Config, import("fumadocs-mdx/runtime/types").Inter
   }
 }>();
 const browserCollections = {
-  docs: create.doc("docs", {"faq.mdx": () => import("../content/docs/faq.mdx?collection=docs"), "index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "discord/discord.mdx": () => import("../content/docs/discord/discord.mdx?collection=docs"), "odyssey/channel.mdx": () => import("../content/docs/odyssey/channel.mdx?collection=docs"), "odyssey/odyssey.mdx": () => import("../content/docs/odyssey/odyssey.mdx?collection=docs"), }),
+  docs: create.doc("docs", {"faq.mdx": () => import("../src/content/docs/faq.mdx?collection=docs"), "index.mdx": () => import("../src/content/docs/index.mdx?collection=docs"), "discord/discord.mdx": () => import("../src/content/docs/discord/discord.mdx?collection=docs"), "odyssey/channel.mdx": () => import("../src/content/docs/odyssey/channel.mdx?collection=docs"), "odyssey/odyssey.mdx": () => import("../src/content/docs/odyssey/odyssey.mdx?collection=docs"), }),
 };
 export default browserCollections;
--- apps/web/.source/server.ts
+++ apps/web/.source/server.ts
@@ -1,10 +1,10 @@
 // @ts-nocheck
-import * as __fd_glob_5 from "../content/docs/odyssey/odyssey.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/odyssey/channel.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/discord/discord.mdx?collection=docs"
-import * as __fd_glob_2 from "../content/docs/index.mdx?collection=docs"
-import * as __fd_glob_1 from "../content/docs/faq.mdx?collection=docs"
-import { default as __fd_glob_0 } from "../content/docs/meta.json?collection=docs"
+import * as __fd_glob_5 from "../src/content/docs/odyssey/odyssey.mdx?collection=docs"
+import * as __fd_glob_4 from "../src/content/docs/odyssey/channel.mdx?collection=docs"
+import * as __fd_glob_3 from "../src/content/docs/discord/discord.mdx?collection=docs"
+import * as __fd_glob_2 from "../src/content/docs/index.mdx?collection=docs"
+import * as __fd_glob_1 from "../src/content/docs/faq.mdx?collection=docs"
+import { default as __fd_glob_0 } from "../src/content/docs/meta.json?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
 
@@ -13,4 +13,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>();
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, }, {"faq.mdx": __fd_glob_1, "index.mdx": __fd_glob_2, "discord/discord.mdx": __fd_glob_3, "odyssey/channel.mdx": __fd_glob_4, "odyssey/odyssey.mdx": __fd_glob_5, });
+export const docs = await create.docs("docs", "src/content/docs", {"meta.json": __fd_glob_0, }, {"faq.mdx": __fd_glob_1, "index.mdx": __fd_glob_2, "discord/discord.mdx": __fd_glob_3, "odyssey/channel.mdx": __fd_glob_4, "odyssey/odyssey.mdx": __fd_glob_5, });
